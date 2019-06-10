bb765fbe455fde67071bb4ecc21b572ac245a3ce
justin <jlouie10@gmail.com>
2019-06-10 09:24:30 -0500

    Added friends array to friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,26 @@
+let friends = [
+    {
+        name: 'Ted Mosby',
+        photo: 'https://www.imdb.com/title/tt0460649/'
+    },
+    {
+        name: 'Robin Scherbatsky',
+        photo: 'https://www.imdb.com/title/tt0460649/'
+    },
+    {
+        name: 'Barney Stinson',
+        photo: 'https://www.imdb.com/title/tt0460649/'
+    },
+    {
+        name: 'Lily Aldrin',
+        photo: 'https://www.imdb.com/title/tt0460649/'
+    },
+    {
+        name: 'Marshall Eriksen',
+        photo: 'https://www.imdb.com/title/tt0460649/'
+    }
+];
+
+module.exports = {
+    list: friends
+};
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,38 +1,17 @@
+const friends = require('../data/friends.js');
+
 // API routes container, called by listener
 let routes = app => {
-    let friends = [
-        {
-            name: 'Ted Mosby',
-            photo: 'https://www.imdb.com/title/tt0460649/'
-        },
-        {
-            name: 'Robin Scherbatsky',
-            photo: 'https://www.imdb.com/title/tt0460649/'
-        },
-        {
-            name: 'Barney Stinson',
-            photo: 'https://www.imdb.com/title/tt0460649/'
-        },
-        {
-            name: 'Lily Aldrin',
-            photo: 'https://www.imdb.com/title/tt0460649/'
-        },
-        {
-            name: 'Marshall Eriksen',
-            photo: 'https://www.imdb.com/title/tt0460649/'
-        }
-    ];
-
     // Fetches all friends
     app.get('/api/friends', (req, res) => {
-        return res.json(friends);
+        return res.json(friends.list);
     });
 
     // Creates new friend from survey and returns a match
     app.post('/api/friends', (req, res) => {
         let survey = req.body;
 
-        friends.push(survey);
+        friends.list.push(survey);
         res.json(survey);
     });
 };
