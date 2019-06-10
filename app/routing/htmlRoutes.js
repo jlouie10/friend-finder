e72bf39cd2b1a7b463e48dcb8e86b34798712639
justin <jlouie10@gmail.com>
2019-06-10 08:16:27 -0500

    Added comments

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,8 +1,11 @@
+// HTML routes container, called by listener
 let routes = (path, app) => {
+    // Route that diplays the homepage to the user
     app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/home.html'));
     });
 
+    // Route that displays the survey to the user
     app.get('/survey', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/survey.html'));
     });
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const express = require('express');
-const html = require('./app/routing/htmlRoutes.js')
+const html = require('./app/routing/htmlRoutes.js');
 
 const PORT = process.env.PORT || 8080;
 
 let app = express();
 
+// Listener for routes
 app.listen(PORT, () => {
     console.log('Server listening on: http://localhost:' + PORT);
 
