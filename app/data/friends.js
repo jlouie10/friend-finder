80735a610d896f0449913e1e25aa0d2629add5fa
justin <jlouie10@gmail.com>
2019-06-10 09:56:10 -0500

    Added scores array to friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,23 +1,83 @@
 let friends = [
     {
         name: 'Ted Mosby',
-        photo: 'https://www.imdb.com/title/tt0460649/'
+        photo: 'https://www.imdb.com/title/tt0460649/',
+        scores: [
+            4,
+            3,
+            2,
+            2,
+            5,
+            4,
+            5,
+            1,
+            5,
+            5
+        ]
     },
     {
         name: 'Robin Scherbatsky',
-        photo: 'https://www.imdb.com/title/tt0460649/'
+        photo: 'https://www.imdb.com/title/tt0460649/',
+        scores: [
+            2,
+            4,
+            1,
+            3,
+            2,
+            4,
+            3,
+            3,
+            3,
+            3
+        ]
     },
     {
         name: 'Barney Stinson',
-        photo: 'https://www.imdb.com/title/tt0460649/'
+        photo: 'https://www.imdb.com/title/tt0460649/',
+        scores: [
+            5,
+            1,
+            5,
+            1,
+            1,
+            5,
+            1,
+            1,
+            1,
+            5
+        ]
     },
     {
         name: 'Lily Aldrin',
-        photo: 'https://www.imdb.com/title/tt0460649/'
+        photo: 'https://www.imdb.com/title/tt0460649/',
+        scores: [
+            3,
+            4,
+            4,
+            3,
+            3,
+            1,
+            3,
+            4,
+            4,
+            4
+        ]
     },
     {
         name: 'Marshall Eriksen',
-        photo: 'https://www.imdb.com/title/tt0460649/'
+        photo: 'https://www.imdb.com/title/tt0460649/',
+        scores: [
+            4,
+            2,
+            3,
+            2,
+            5,
+            2,
+            5,
+            1,
+            2,
+            5
+        ]
     }
 ];
 
